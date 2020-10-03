f88d5a4980e7c39d504b3d20636cb101e5503728
Luann Rodrigo <luann.rodrigos@gmail.com>
2020-10-03 17:23:46 -0300

    Commit initial

--- src/components/Top/Keyboard/index.jsx
+++ src/components/Top/Keyboard/index.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
+
 import { Keyboard } from './styles';
 
+const handleChange = () => {
 
-<Keyboard>
-  <div>
-    <button>1</button>
-    <button>2 abc</button>
-    <button>3 def</button>
-  </div>
-  <div>
-    <button>4 ghi</button>
-    <button>5 jkl</button>
-    <button>6 mno</button>
-  </div>
-  <div>
-    <button>7 pqrs</button>
-    <button>8 tuv</button>
-    <button>9 wxyz</button>
-  </div>
-  <div>
-    <button>*</button>
-    <button>0</button>
-    <button>#</button>
-  </div>
-</Keyboard>
+}
+
+const Keyboard = (text) => {
+  return (
+    <Keyboard>
+      <div>
+        <button>1</button>
+        <button>2 abc</button>
+        <button>3 def</button>
+      </div>
+      <div>
+        <button>4 ghi</button>
+        <button>5 jkl</button>
+        <button>6 mno</button>
+      </div>
+      <div>
+        <button>7 pqrs</button>
+        <button>8 tuv</button>
+        <button>9 wxyz</button>
+      </div>
+      <div>
+        <button>*</button>
+        <button>0</button>
+        <button>#</button>
+      </div>
+    </Keyboard>
+  )
+}
+
+
+
+
+
+export default Screen;
